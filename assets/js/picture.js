caf781e09e851992abd7770094b73650b4b5509a
Mauro Druwel <mauro.druwel@gmail.com>
2026-01-22 17:48:31 +0100

    Let picture close when clicking background

--- assets/js/picture.js
+++ assets/js/picture.js
@@ -1,23 +1,42 @@
 var modal = document.getElementById("myModal");
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    var span = document.getElementsByClassName("close")[0];
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
 
-    function openModal(imgSrc, altText) {
-        modal.style.display = "block";
-        modalImg.src = imgSrc;
-        captionText.innerHTML = altText;
-    }
+function openModal(imgSrc, altText) {
+    if (!modal || !modalImg || !captionText) return;
+    modal.style.display = "block";
+    modalImg.src = imgSrc;
+    captionText.innerHTML = altText;
+}
 
-    function closeModal() {
-        modal.style.display = "none";
-    }
+function closeModal() {
+    if (!modal) return;
+    modal.style.display = "none";
+}
 
-    var images = document.querySelectorAll(".modal-img");
+window.initializeModals = function () {
+    const images = document.querySelectorAll(".modal-img");
     images.forEach(function (img) {
+        // Prevent duplicate listeners
+        if (img.dataset.hasListener) return;
         img.addEventListener("click", function () {
             openModal(img.src, img.alt);
         });
+        img.dataset.hasListener = "true";
     });
+};
 
-    span.onclick = closeModal;
+document.addEventListener('DOMContentLoaded', () => {
+    window.initializeModals();
+    if (span) span.onclick = closeModal;
+
+    // Close modal when clicking on the background
+    if (modal) {
+        modal.onclick = function (event) {
+            if (event.target === modal) {
+                closeModal();
+            }
+        };
+    }
+});
